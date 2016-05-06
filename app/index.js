2924125d59eacb4ab95e9d7809d1a9c8f6bd75cf
coreyc <ccleary00@gmail.com>
2016-05-05 21:33:44 -0400

    added user prompting

--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
 var generators = require('yeoman-generator');
 
-module.exports = generators.Base.extend();
+module.exports = generators.Base.extend({
+    prompting: function () {
+        return this.prompt({
+            plugin: 'plugin name',
+            description: 'description',
+            author: 'author name',
+            default: this.appname
+        }).then(function (answers) {
+            this.log(answers);
+        }).bind(this);
+    }
+});
